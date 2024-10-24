22f59aeef174fc9db37a11ad02c8a9f25df9e836
sushixi <sushixi@deyran.eu.org>
2024-10-25 01:22:09 +0800

    解决编译报错问题

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  transpileDependencies: true,
+  lintOnSave: false,
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'deyran——我的博客'
+        return args
+      })
+    plugins: [vue()]
+  }
 })
