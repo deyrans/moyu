fbf26df8cedeb529f10638f2e6f41752342d9ada
sushixi <sushixi@deyran.eu.org>
2024-10-25 00:47:54 +0800

    新增ElementPlus组件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'dayjs/locale/zh-cn'e
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.use(ElementPlus, {
+    locale: zhCn,
+})
+app.mount('#app')
