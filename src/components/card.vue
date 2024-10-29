932cd04acbb8eb412f4e54e0251c6f5445bdbb1b
sushixi <sushixi@deyran.eu.org>
2024-10-30 01:51:04 +0800

    界面布局2

---
+++ src/components/card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="card"></div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+#card {
+  margin: 2px;
+  margin: auto;
+  background-color: brown;
+  border-radius: 10px;
+  min-height: 20pc;
+  min-width: 15pc;
+}
+</style>
