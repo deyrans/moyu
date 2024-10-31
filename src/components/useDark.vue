d90daf7e59cc21399fdc1c982a344c70c291b338
sushixi <sushixi@deyran.eu.org>
2024-11-01 03:12:32 +0800

    auto模式下，黑夜功能异常

--- src/components/useDark.vue
+++ src/components/useDark.vue
@@ -4,17 +4,19 @@ import { useDark, useToggle } from "@vueuse/core";
 import { Sunny, Moon } from "@element-plus/icons-vue";
 
 const is_switchDark = ref<boolean>(
-  localStorage.getItem("useDarkKEY") === "dark" ? true : false
+  localStorage.getItem("vitepress-theme-appearance") === "dark" ? true : false
 );
 // useDark 来检测当前是否为深色模式，并通过 useToggle 来切换深色与浅色模式
 const isDark = useDark({
-  // 存储到localStorage中的Key 根据自己的需求更改
-  storageKey: "useDarkKEY",
+  storageKey: 'vitepress-theme-appearance',
   // 暗黑class名字
   valueDark: "dark",
   // 高亮class名字
   valueLight: "light",
 });
+
+console.log(localStorage.getItem("vitepress-theme-appearance"));
+
 const toggleDark = useToggle(isDark);
 </script>
 
